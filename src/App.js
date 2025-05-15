ea371e1fefba6fedeb426bc0f04616eb6b128189
Arzu Caner <arzuguneycaner@gmail.com>
2025-05-15 20:48:29 +0100

    Update App.js to integrate new components and pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import JavaScript from './pages/JavaScript';
+import ReactPage from './pages/React';
+import ReactNative from './pages/ReactNative';
+import TypeScript from './pages/TypeScript';
+import NodeJS from './pages/NodeJS';
+import HTML from './pages/HTML';
+import CSS from './pages/CSS';
 import './App.css';
 
 function App() {
@@ -11,7 +18,14 @@ function App() {
         <Navbar />
         <main className="main-content">
           <Routes>
-            <Route path="/" element={<Home />} />         
+            <Route path="/" element={<Home />} />
+            <Route path="/javascript" element={<JavaScript />} />
+            <Route path="/react" element={<ReactPage />} />
+            <Route path="/react-native" element={<ReactNative />} />
+            <Route path="/typescript" element={<TypeScript />} />
+            <Route path="/nodejs" element={<NodeJS />} />
+            <Route path="/html" element={<HTML />} />
+            <Route path="/css" element={<CSS />} />
             <Route path="*" element={<div>Page not found</div>} />
           </Routes>
         </main>
